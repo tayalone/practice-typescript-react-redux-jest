bafadcd6fa16c1723a91a85c22c536f956c13bdc
estellar_boyz <tayalone@gmail.com>
2020-01-17 01:06:14 +0700

    :sparkles: setup test enviroment success

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  snapshotSerializers: ['enzyme-to-json/serializer']
+}
---
+++ src/Components/Link/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface LinkProps {
+  className?: string
+  href: string
+}
+
+const Link: React.FC<LinkProps> = ({ className, href, children }) => {
+  return (
+    <a href={href} className={className}>
+      {children}
+    </a>
+  )
+}
+
+export default Link
